25bb322e8107da99fbc98f7cf38b7e20a4565557
xSourcerer <saqibgamer307@gmail.com>
2024-09-10 19:12:19 +0500

    Remove all the repos before resyncing

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #! /bin/sh
 
 rm -rf .repo/local_manifests
+rm -rf vendor/xiaomi/surya
+rm -rf vendor/xiaomi/sm6150-common
+rm -rf device/xiaomi/surya
+rm -rf device/xiaomi/sm6150-common
+rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/miuicamera
 
 repo init -u https://github.com/Evolution-X/manifest.git --depth 1 -b udc --git-lfs
-
-/opt/crave/resync.sh
-
 
 git clone https://github.com/xSourcerer/local_manifests --depth 1 -b evo .repo/local_manifests
 
