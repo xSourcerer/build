0dbe32c49dd346033314e5fbf6f00cbc0b604498
xSourcerer <saqibgamer307@gmail.com>
2024-09-11 08:50:31 +0500

    Fixed branch name

--- build.sh
+++ build.sh
@@ -2,17 +2,14 @@
 
 rm -rf .repo/local_manifests
 
-rm -rf kernel/xiaomi/surya
-rm -rf vendor/xiaomi/surya
-rm -rf vendor/xiaomi/sm6150-common
-rm -rf device/xiaomi/surya
-rm -rf device/xiaomi/sm6150-common
+rm -rf kernel/xiaomi/*
+rm -rf vendor/xiaomi/*
+rm -rf device/xiaomi/*
 rm -rf hardware/xiaomi
-rm -rf vendor/xiaomi/miuicamera
 
 repo init -u https://github.com/Evolution-X/manifest.git --depth 1 -b udc --git-lfs
 
-git clone https://github.com/xSourcerer/local_manifests --depth 1 -b crdroid .repo/local_manifests
+git clone https://github.com/xSourcerer/local_manifests --depth 1 -b crd .repo/local_manifests
 
 /opt/crave/resync.sh
 
