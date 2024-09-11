23f65c84a5c51b24749b84086799616ea1ca1a5f
xSourcerer <saqibgamer307@gmail.com>
2024-09-11 08:37:18 +0500

    crdroid variant of manifest

--- build.sh
+++ build.sh
@@ -12,20 +12,12 @@ rm -rf vendor/xiaomi/miuicamera
 
 repo init -u https://github.com/Evolution-X/manifest.git --depth 1 -b udc --git-lfs
 
-git clone https://github.com/xSourcerer/local_manifests --depth 1 -b evo .repo/local_manifests
+git clone https://github.com/xSourcerer/local_manifests --depth 1 -b crdroid .repo/local_manifests
 
 /opt/crave/resync.sh
 
 . build/envsetup.sh
 
 lunch lineage_surya-userdebug
-
-rm -rf vendor/evolution-priv/keys
-
-croot && git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
-
-cd vendor/evolution-priv/keys
-./keys.sh
-croot
 
 m evolution
